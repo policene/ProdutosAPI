acc193cd794a32ca1feb5f3ea0cfb762b2ee909a
Giovanni Policene <giovannicpoli@gmail.com>
2025-03-26 20:27:12 -0300

    feat: rotas post, put e delete

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.use(express.json());
 
 const produtos = [];
 
-// Rota get.
+// Rota GET.
 app.get('/produtos', async (req, res) => {
 
     try {
-        const result = await pool.query('SELECT * FROM products');
+        const result = await pool.query('SELECT * FROM products ORDER BY id ASC');
         res.status(200).json(result.rows);
     } catch (err) {
         console.log(err);
@@ -20,10 +20,39 @@ app.get('/produtos', async (req, res) => {
     }
 });
 
+// Rota POST.
 app.post('/produtos', (req, res) => {
-    produtos.push(req.body);
-    res.status(201).json(req.body);
+
+    const { nome, fornecedor, endereco_fornecedor, quantidade, endereco, preco_unitario } = req.body;
+
+    pool.query('INSERT INTO products (nome, fornecedor, endereco_fornecedor, quantidade, endereco, preco_unitario) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', 
+        [nome, fornecedor, endereco_fornecedor, quantidade, endereco, preco_unitario]
+    )
+
+    res.status(201).send(req.body)
+
 });
+
+// Rota PUT.
+app.put('/produtos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { nome, fornecedor, endereco_fornecedor, quantidade, endereco, preco_unitario } = req.body;
+
+    pool.query('UPDATE products SET nome = $1, fornecedor = $2, endereco_fornecedor = $3, quantidade = $4, endereco = $5, preco_unitario = $6 WHERE id = $7',
+        [nome, fornecedor, endereco_fornecedor, quantidade, endereco, preco_unitario, id]
+    )
+
+    res.status(200).send("Produto alterado no id: " + id)
+
+})
+
+// Rota DELETE.
+app.delete('/produtos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query('DELETE FROM products WHERE id = $1', [id])
+    res.status(200).send("O produto com id " + id + " foi deletado");
+})
 
 
 // Iniciar o servidor
